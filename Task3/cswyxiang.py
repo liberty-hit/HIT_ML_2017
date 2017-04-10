4371c6764b01e4ae4b8033f987e289f82596c441
cswyxiang <1353281061@qq.com>
2017-04-11 00:16:39 +0800

    修改后的Task3

--- Task3/cswyxiang.py
+++ Task3/cswyxiang.py
@@ -10,58 +10,42 @@ dataY = file2.readlines()
 file1.close()
 file2.close()
 
-X1 = []
-X2 = []
+X = []
 Y = []
 
 for eachline in dataX:
-	X1.append(float(eachline.split()[0]))
-	X2.append(float(eachline.split()[1]))
+	X1 = float(eachline.split()[0])
+	X2 = float(eachline.split()[1])
+	X.append((1,X1,X2,X1*X2,X1**2,X2**2,X1*X2**2,X2*X1**2,X1**3,X2**3,X1*X2**3,X2*X1**3,X1**2*X2**2,X1**4,X2**4))
 for eachline in dataY:
 	Y.append(float(eachline))
 
-def h(x1,x2,theta):
-	return theta[0] + theta[1]*x1 + theta[2]*x2 + theta[3]*x1**2 + theta[4]*x2**2 + theta[5]*x1**3 + theta[6]*x2**3 + theta[7]*x1*x2 + theta[8]*x1*x2**2 + theta[9]*x2*x1**2 + theta[10]*x1*x2**3+theta[11]*x2*x1**3 + theta[12]*x2**2*x1**2 +theta[13]*x1**4+ theta[14]*x2**4
+X = mat(X)
+Y = mat(Y).T
 
 theta = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+theta = mat(theta).T
 m = size(X1)
 alpha = 0.01
-Max_step = 60000
+Max_step = 80000
 
 for i in range(Max_step):
-	sigma = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	for j in range(m):
-		sigma[0] = sigma[0] + 1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j] 
-		sigma[1] = sigma[1] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X1[j]
-		sigma[2] = sigma[2] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X2[j]
-		sigma[3] = sigma[3] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X1[j]**2
-		sigma[4] = sigma[4] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X2[j]**2
-		sigma[5] = sigma[5] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X1[j]**3
-		sigma[6] = sigma[6] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X2[j]**3
-		sigma[7] = sigma[7] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X2[j]*X1[j]
-		sigma[8] = sigma[8] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X1[j]*X2[j]**2
-		sigma[9] = sigma[9] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X2[j]*X1[j]**2
-		sigma[10] = sigma[10] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X1[j]*X2[j]**3
-		sigma[11] = sigma[11] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X2[j]*X1[j]**3
-		sigma[12] = sigma[12] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X1[j]**2*X2[j]**2
-		sigma[13] = sigma[13] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X1[j]**4
-		sigma[14] = sigma[14] + (1/(1+exp(-h(X1[j],X2[j],theta)))-Y[j])*X2[j]**4
-	for k in range(15):
-		theta[k] = theta[k] - alpha*(1/m)*sigma[k]
-
+	error = 1/(1 + exp(-X*theta)) - Y
+	theta = theta - alpha * X.T * error
+print(theta)
 x1 = np.arange(-5, 9, .01)
 x2 = np.arange(-5, 5, .01)
 x1,x2 = np.meshgrid(x1,x2)
-y = h(x1,x2,theta)
+f = theta[0,0] + theta[1,0]*x1 + theta[2,0]*x2 + theta[3,0]*x1*x2 + theta[4,0]*x1**2 + theta[5,0]*x2**2 + theta[6,0]*x1*x2**2 + theta[7,0]*x2*x1**2 + theta[8,0]*x1**3 + theta[9,0]*x2**3 + theta[10,0]*x1*x2**3 + theta[11,0]*x2*x1**3 + theta[12,0]*(x1**2)*(x2**2) + theta[13,0]*x1**4 + theta[14,0]*x2**4
 
-plt.contour(x1,x2,y,0)
+plt.contour(x1,x2,f,0)
 
 t = 0;
 for each in Y:	
-	if float(each) > 0:
-		plt.scatter(X1[t],X2[t],color = 'red',marker = "x")
+	if each > 0:
+		plt.scatter(X[t,1],X[t,2],color = 'red',marker = "x")
 	else:
-		plt.scatter(X1[t],X2[t],color = 'blue')
+		plt.scatter(X[t,1],X[t,2],color = 'blue')
 	t = t + 1
 plt.title("cswyxiang's Graphics")
 plt.legend()
